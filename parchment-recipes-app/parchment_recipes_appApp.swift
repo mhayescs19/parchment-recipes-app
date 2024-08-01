1e3aff3c69614f0298f27ef1c8f735c59997ef6a
mhayescs19 <mhayescs19@gmail.com>
2024-08-01 16:23:32 -0700

    Initial Commit

---
+++ parchment-recipes-app/parchment_recipes_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  parchment_recipes_appApp.swift
+//  parchment-recipes-app
+//
+//  Created by Michael Hayes on 8/1/24.
+//
+
+import SwiftUI
+
+@main
+struct parchment_recipes_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
